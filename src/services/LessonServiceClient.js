68ff98da4310af1bdfd806286cff6cd5d38ff275
jiayiliu <liu.jiayi@husky.neu.edu>
2018-06-06 16:50:32 -0700

    modified url

--- src/services/LessonServiceClient.js
+++ src/services/LessonServiceClient.js
@@ -1,7 +1,7 @@
 let _singleton = Symbol();
 
 const MODULE_API_URL =
-    'https://webdev-summer-assignment.herokuapp.com/course/CID/module';
+    'https://webdev-summer-assignment.herokuapp.com/api/course/CID/module';
 
 export default class LessonServiceClient {
     constructor(singletonToken) {
@@ -10,14 +10,14 @@ export default class LessonServiceClient {
     }
 
     findAllLessonsForModule(courseId,moduleId) {
-        return fetch('https://webdev-summer-assignment.herokuapp.com/course'+'/'+courseId+'/module/'+moduleId+'/lesson')
+        return fetch('https://webdev-summer-assignment.herokuapp.com/api/course'+'/'+courseId+'/module/'+moduleId+'/lesson')
             .then(function(response) {
                 return response.json();
             })
     }
 
     createLesson(courseId, moduleId,lesson) {
-        return fetch('https://webdev-summer-assignment.herokuapp.com/course/'+courseId+'/module/'+moduleId+'/lesson',
+        return fetch('https://webdev-summer-assignment.herokuapp.com/api/course/'+courseId+'/module/'+moduleId+'/lesson',
             {   body: JSON.stringify(lesson),
                 headers: { 'Content-Type': 'application/json' },
                 method: 'POST'
@@ -26,7 +26,7 @@ export default class LessonServiceClient {
     }
 
     deleteLesson(lessonId){
-        return fetch('https://webdev-summer-assignment.herokuapp.com/lesson'+'/'+lessonId,
+        return fetch('https://webdev-summer-assignment.herokuapp.com/api/lesson'+'/'+lessonId,
             {
                 method: 'DELETE'
             }).then(function (response) {
--- src/services/ModuleServiceClient.js
+++ src/services/ModuleServiceClient.js
@@ -1,7 +1,7 @@
 
 let _singleton = Symbol();
 
-const MODULE_API_URL = 'https://webdev-summer-assignment.herokuapp.com/course/CID/module';
+const MODULE_API_URL = 'https://webdev-summer-assignment.herokuapp.com/api/course/CID/module';
 
 
 export default class ModuleServiceClient {
@@ -29,7 +29,7 @@ export default class ModuleServiceClient {
 
     deleteModule(moduleId) {
        // return fetch(MODULE_API_URL + '/' + moduleId,
-        return fetch('https://webdev-summer-assignment.herokuapp.com/module'+'/'+moduleId,
+        return fetch('https://webdev-summer-assignment.herokuapp.com/api/module'+'/'+moduleId,
             {
                 method: 'DELETE'
             }).then(function (response) {
@@ -38,7 +38,7 @@ export default class ModuleServiceClient {
     }
 
     findAllModules(){
-        return fetch('https://webdev-summer-assignment.herokuapp.com/module')
+        return fetch('https://webdev-summer-assignment.herokuapp.com/api/module')
             .then(function(response){
                 return response.json();
             });
